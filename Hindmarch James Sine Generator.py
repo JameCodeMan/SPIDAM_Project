bfb9a4352bb18f343d3c5cfbb8787a10b69b2d36
JameCodeMan <JCH.2018.66@gmail.com>
2024-11-21 02:03:28 -0500

    Messed with plot frequency
    Done because the assignment asked for this

--- Hindmarch James Sine Generator.py
+++ Hindmarch James Sine Generator.py
@@ -4,8 +4,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x=np.arange(0, 5*np.pi, 0.1)
-y=np.sin(x)
+x=np.arange(0, 10*np.pi, 0.1)
+y=np.sin(2*x)
 
 plt.plot(x,y, color="green")
 plt.show()
