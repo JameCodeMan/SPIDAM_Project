307b8e80cd443abfdb612dad3fce4e0763711a7e
JameCodeMan <JCH.2018.66@gmail.com>
2024-12-07 03:59:25 -0500

    Committed at 3:59 a.m by James Hindmarch (jhindmarch4017@floridapoly.edu)
    Updates core functionality module to return plots from function rather than simply display them from the function
    Done to (hopefully) simplify integration with GUI element
    
    Also altered the WavPlt function to accomadate audio data with varying numbers of channels,
    however only 2-channel audio seems to give good results, as 1-channel and 4-channel audio files do not produce good looking graphs

--- SPIDAM_Main.py
+++ SPIDAM_Main.py
@@ -2,6 +2,7 @@ from scipy.io import wavfile
 import scipy.io
 import matplotlib.pyplot as plt
 import numpy as np
+import tkinter
 
 class WavObj:
     def __init__(self, SampRt, Data, Length, Time):
@@ -9,31 +10,27 @@ class WavObj:
         self.Data=Data
         self.Length=Length
         self.Time=Time
-        self.spectrum, self.freqs, self.t, self.im = plt.specgram(self.Data, Fs=self.SampRt, NFFT=1024)
+        #self.spectrum, self.freqs, self.t, self.im = plt.specgram(self.Data, Fs=self.SampRt, NFFT=1024)
         self.TrgtFreq=0
+        self.figcount=1
     
-    #Only Works with 2 Channel audio, will crash if given .wav file with different number of channels
+    #Plots 2-channel audio data very well
+    #Will plot 4-channel data and single channel data, however the graph in both cases will be strange and seemingly worthless
     def WavPlt(self):
-        plt.plot(self.Time, self.Data[:,0], label="Left channel")
-        plt.plot(self.Time, self.Data[:, 1], label="Right channel")
+        plt.close()
+        Channels=self.Data.shape[len(self.Data.shape) - 1]
+        try:
+            for x in range(Channels):
+                plt.plot(self.Time, self.Data[:,x], label=f"Channel {x+1}")
+        except:
+            plt.plot(self.Time, self.Data[:], label="Channel 1")
         plt.legend()
         plt.xlabel("Time [s]")
         plt.ylabel("Amplitude")
-        plt.show()
-    
-    #Currently seems useless, will remove if no purpose found
-    def SpcGrm(self):
-        spectrum, freqs, t, im = plt.specgram(self.Data, Fs=self.SampRt, NFFT=1024, cmap=plt.get_cmap('autumn_r'))
-    
-    #Currently only works with single channel audio, plots intensity relative to frequency and time
-    def FreqAmp(self):
-        plt.specgram(self.Data, Fs=self.SampRt, NFFT=1024, cmap=plt.get_cmap('autumn_r'))
-        cbar=plt.colorbar(self.im)
-        plt.xlabel('Time (s)')
-        plt.ylabel('Frequency (HZ)')
-        cbar.set_label('Intensity (dB)')
-        plt.show()
-    
+        temp=plt
+        return temp
+        #plt.show()
+
     #Used by other functions to determine frequency 
     def FreqGet(self, freqs):
         for x in freqs:
@@ -41,8 +38,20 @@ class WavObj:
                 break
         return x
     
+    #Currently only works with single channel audio, plots intensity relative to frequency and time
+    def FreqAmp(self):
+        plt.close()
+        no, noo, noooo, im=plt.specgram(self.Data, Fs=self.SampRt, NFFT=1024, cmap=plt.get_cmap('autumn_r'))
+        cbar=plt.colorbar(im)
+        plt.xlabel('Time (s)')
+        plt.ylabel('Frequency (HZ)')
+        cbar.set_label('Intensity (dB)')
+        temp=plt
+        return temp
+    
     #Currently only works on single channel audio (probably), not sure exactly what it does but it seems to return some sort of measurement in decibels 
     def FreqChck(self):
+        plt.close()
         self.TrgtFreq=self.FreqGet(self.freqs)
         FreqIndx=np.where(self.freqs == self.TrgtFreq)[0][0]
         FreqData=self.spectrum[FreqIndx]
@@ -51,15 +60,22 @@ class WavObj:
     
     #Currently only works on single channel audio (probably), plots decibel levels as a fucntion of time
     def FreqPlot(self):
+        plt.close()
         DbData=self.FreqChck()
-        plt.figure(2)
+        plt.figure(self.figcount)
+        self.figcount+=1
         plt.plot(self.t, DbData, linewidth=1, alpha=0.7, color='#004bc6')
         plt.xlabel('Time (s)')
         plt.ylabel('Power (dB)')
-        plt.show()
+        temp=plt
+        return temp
+        #plt.show()
      
     #Currently only works on single channel audio (probably), computes rt20 and rt60 values
     def RvrbMesr(self):
+        plt.close()
+        plt.figure(self.figcount)
+        self.figcount+=1
         DbData=self.FreqChck()
         MaxIndx=np.argmax(DbData)
         MaxVal=DbData[MaxIndx]
@@ -77,9 +93,11 @@ class WavObj:
         rt60=3*rt20
         #plt.xlim(0, ((round(abs(rt60), 2))*1.5))
         plt.grid()
-        plt.show()
+        temp=plt
         self.TrgtFreq=self.FreqGet(self.freqs)
-        print(f"The RT60 reverb time at freq {int(self.TrgtFreq)}Hz is {round(abs(rt60), 2)} seconds")
+        #plt.show()
+        #print(f"The RT60 reverb time at freq {int(self.TrgtFreq)}Hz is {round(abs(rt60), 2)} seconds")
+        return temp, rt60
     
     def FindNearest(self, array, value):
         array=np.asarray(array)
@@ -97,11 +115,16 @@ def main():
     wavfilepath=input("Input the absolute path of your .wav file: ")
     tempSamp, tempData, tempLength, tempTime=WavStat(wavfilepath)
     MyWavObj=WavObj(tempSamp, tempData, tempLength, tempTime)
-    #MyWavObj.WavPlt() #Doesn't work with audio that isn't 2-channel, will be fixed soon
-    #MyWavObj.SpcGrm() #Doesn't do much, will be removed soon
-    MyWavObj.FreqAmp() 
-    MyWavObj.FreqPlot()
-    MyWavObj.RvrbMesr()
-    
+    temp=MyWavObj.WavPlt() #Doesn't work with audio that isn't 2-channel, will be fixed soon
+    temp.show()
+    """
+    temp1=MyWavObj.FreqPlot()
+    temp1.show()
+    plot, rt60=MyWavObj.RvrbMesr()
+    plot.show()
+    print(f"The RT60 reverb time at freq {int(MyWavObj.TrgtFreq)}Hz is {round(abs(rt60), 2)} seconds")
+    temp=MyWavObj.FreqAmp()
+    temp.show()
+    """
 if __name__=="__main__":
     main()
